dbaf97d3715deea30dbf8694055ea31c80328f29
ElvishPiper <81609085+ElvishPiper@users.noreply.github.com>
2021-05-26 20:55:42 +0800

    Add files via upload

--- submitted.php
+++ submitted.php
@@ -5,6 +5,7 @@
   <title>Newsletter</title>
   <link rel="stylesheet" href="styles.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 
 <body class="grid-container">
